e4f391dfe464b8f12c0219d7fb59c64ed1fe180c
F5 <you@example.com>
2025-09-15 10:56:21 -0300

    estimaciones implementadas

--- blueprints/estimaciones.py
+++ blueprints/estimaciones.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from database import get_db_connection
+from utils.db import get_db_connection
 import logging
 
 # Configurar logging
