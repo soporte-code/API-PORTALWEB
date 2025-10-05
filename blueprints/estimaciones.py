f7d591ea7a7a16950c02cd44809e108c8ae2a089
F5 <you@example.com>
2025-10-05 18:33:45 -0300

    fix: agregar campo hora_registro al INSERT de rendimiento-packing

--- blueprints/estimaciones.py
+++ blueprints/estimaciones.py
@@ -1928,8 +1928,8 @@ def crear_rendimiento_packing_cuartel(cuartel_id):
         # Insertar nuevo rendimiento packing
         insert_query = """
             INSERT INTO estimacion_fact_rendimientocuartel (
-                id, rendimiento, fecha, id_usuario, id_cuartel
-            ) VALUES (%s, %s, %s, %s, %s)
+                id, rendimiento, fecha, id_usuario, id_cuartel, hora_registro
+            ) VALUES (%s, %s, %s, %s, %s, NOW())
         """
         
         cursor.execute(insert_query, (
